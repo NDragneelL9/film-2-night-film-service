3ae127c5af827f566a6738c1f271a0a3dc4de760
NDragneelL9 <nell.dragneel@gmail.com>
2023-05-25 16:59:31 +0300

    #8 done

---
+++ src/main/java/com/timfralou/app/servlets/HttpServlet.java
@@ -0,0 +1,15 @@
+package com.timfralou.app.servlets;
+
+import java.io.IOException;
+
+public class HttpServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request,
+            javax.servlet.http.HttpServletResponse response) throws IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+            throws IOException {
+
+    }
+}
