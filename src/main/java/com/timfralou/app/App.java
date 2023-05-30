270cacfaedbe1f62ac41056200a2fca3df5d6f44
NDragneelL9 <nell.dragneel@gmail.com>
2023-05-30 15:40:47 +0300

    #2 done

--- src/main/java/com/timfralou/app/App.java
+++ src/main/java/com/timfralou/app/App.java
@@ -10,9 +10,10 @@ import java.sql.ResultSet;
  */
 public class App {
     public static void main(String[] args) {
-        String DBurl = "jdbc:postgresql://localhost:5432/film2night";
-        String DBuser = "f2n_admin";
-        String DBpass = "film2night";
+        String DBurl = "jdbc:postgresql://PostgreSQL/";
+        String DBuser = "postgres";
+        String DBpass = "postgres";
+        System.out.println("App started");
         PostgreDB postgreDB = new PostgreDB(DBurl, DBuser, DBpass);
         try (ResultSet rs = postgreDB.selectQuery("SELECT 5;")) {
             while (rs.next()) {
@@ -20,6 +21,10 @@ public class App {
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
+        }
+        System.out.println("App running...");
+        while (true) {
+
         }
     }
 }
